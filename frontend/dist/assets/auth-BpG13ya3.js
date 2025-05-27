6c04a58a50becc9155d80a7b50421d92b3bd4697
Ashish Joshi <97600247+AJTITAN@users.noreply.github.com>
2025-05-27 14:11:56 +0000

    adding assests

---
+++ frontend/dist/assets/auth-BpG13ya3.js
@@ -0,0 +1,1 @@
+import{R as a,l as i,b as o,r as n,j as s,O as r}from"./index-C2qyRJXm.js";function x(){const t=a(i),e=o();return n.useEffect(()=>{t||e("/")},[t]),t?s.jsx(r,{}):s.jsx("div",{})}export{x as default};
